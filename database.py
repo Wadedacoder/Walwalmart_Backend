dd799a0fd2097a7e9497374fc49941e84df59991
Wadedacoder <devm4966@gmail.com>
2023-03-18 22:11:23 +0530

    Testing

---
+++ database.py
@@ -0,0 +1,50 @@
+import mysql.connector
+import json
+import os
+
+env_var = os.environ
+
+
+class Database:
+    def __init__(self):
+        self.cnx = mysql.connector.connect(
+            host=env_var["HOST"],
+            user=env_var["USER"],
+            password=env_var["PASSWORD"],
+            database=env_var["DATABASE"],
+            port=env_var["PORT"]
+        )
+        if(self.cnx):
+            print("Connected to database ", env_var["DATABASE"])
+        self.cursor = self.cnx.cursor(dictionary=True)
+
+    def run_select_query(self, query, args=None) -> str:
+        self.cursor.execute(query, args)
+        result = self.cursor.fetchall()
+        # print(result, type(result), "result")
+        return json.dumps(result)
+
+    def run_insert_query(self, query, args=None) -> bool:
+        try:
+            self.cursor.execute(query, args)
+            self.cnx.commit()
+            return True
+        except:
+            return False
+
+    def run_update_query(self, query, args=None) -> bool:
+        try:
+            self.cursor.execute(query, args)
+            self.cnx.commit()
+            return True
+        except:
+            return False
+
+    def run_delete_query(self, query, args=None) -> bool:
+        try:
+            self.cursor.execute(query, args)
+            self.cnx.commit()
+            return True
+        except:
+            return False
+
--- main.py
+++ main.py
@@ -1,40 +1,26 @@
 from fastapi import FastAPI
 import uvicorn
 import os
-# from config import settings
-import mysql.connector
 import json
+from database import Database
+# from config import settings
+
 #
 app = FastAPI()
-# cnx = mysql.connector.connect(
-#     host='localhost',
-#     user='root',
-#     password='dev21038',
-#     database='Walwalmart',
-#     )
-# cursor = cnx.cursor(dictionary=True)
-# query_select = ("SELECT * FROM admins")
-# query_select2 = ("SELECT * FROM users WHERE username = %s")
+database = Database()
+query_select = ("SELECT * FROM admins ; ")
+query_select2 = ("SELECT * FROM users WHERE username = %s;")
+
 
 @app.get("/")
 async def root():
-    # cursor.execute(query_select)
-    # tmp = cursor.fetchall()
-    # tmp = json.dumps(tmp)
-    # print(tmp)
-    # sh = json.loads(tmp)
-    # print(sh)
-    return "hi"
+    return {"message": "Hello World"}
 
 
-# @app.get("/hello/{name}")
-# async def say_hello(name: str):
-#     cursor.execute(query_select2, (name,))
-#     tmp = cursor.fetchall()
-#     tmp = json.dumps(tmp)
-#     # return {"message": f"Hello {name}"}
-#     return tmp
+@app.get("/admins")
+async def say_hello():
+    result = database.run_select_query(query_select)
+    return result
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
-
+    uvicorn.run("main:app", host="localhost", port=5000, log_level="info")
